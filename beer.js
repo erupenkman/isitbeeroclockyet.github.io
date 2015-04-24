8be881b8f89613cad6bc948d9383e899766be4b5
Alister Scott <alister.scott@gmail.com>
2015-04-24 15:48:23 +1000

    initial

---
+++ beer.js
@@ -0,0 +1,33 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+
+window.onload = function () {
+	if (getCookie("visited") == "true")
+	{
+		window.setTimeout(function(){
+        window.location.href = "http://www.aa.org.au/";
+    	}, 3000);
+	}
+	else
+	{
+		var date = new Date;
+		date.getTime();
+		var hour = date.getHours();
+		var minute = date.getMinutes();
+		var second = date.getSeconds();
+		if (hour == "16" && minute == "30")
+		{
+			document.querySelector('#message').textContent = "YES";
+		}
+    	document.cookie="visited=true";
+	}
+};
