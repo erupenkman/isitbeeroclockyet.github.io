5881fa707b6553bc303060d910896efd8ff917d6
Alister Scott <alister.scott@gmail.com>
2015-05-01 16:17:27 +1000

    amazing dance

--- beer.js
+++ beer.js
@@ -1,25 +1,16 @@
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
-
-
 window.onload = function () {
+	setInterval(function () {
 		var date = new Date;
 		date.getTime();
 		var hour = date.getHours();
 		var minute = date.getMinutes();
 		var second = date.getSeconds();
+
 		if (hour == "16" && minute >= 30 && date.getDay() === 5)
 		{
 			document.querySelector('#message').textContent = "YES";
       		document.querySelector('#world').style.display = 'block';
+      		document.querySelector('#pic').src = "dance.gif";
       	}
-	}
+	}, 1000);
 };
